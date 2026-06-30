98da6b76a3a35b7421c3e8b934ff69abf086de5c
Curtis Izuchukwu <izuchukwucur@gmail.com>
2026-06-30 20:22:35 +0100

    Add reflection and topic stats tests

--- src/main/java/com/pararepilot/ui/controller/TopicDetailController.java
+++ src/main/java/com/pararepilot/ui/controller/TopicDetailController.java
@@ -191,6 +191,7 @@ public class TopicDetailController {
 
             stage.setScene(scene);
             stage.showAndWait();
+            
             loadWorksheets();
 
         } catch (IOException e) {
